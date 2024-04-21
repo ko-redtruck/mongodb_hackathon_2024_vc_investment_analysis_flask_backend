fb8e0b6f634ee0eabc92d1eaf60dbdce5c679626
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2024-04-20 20:29:30 -0700

    deploy

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from flask import render_template
 from pymongo import MongoClient
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 ATLAS_URI = "mongodb+srv://kettererkonstantin:7GNfXDPi9Z5SpOyP@hackathon0.1wyzy.mongodb.net/?retryWrites=true&w=majority&appName=hackathon0"
 
@@ -50,11 +52,16 @@ import random
 @app.route("/analyse_startup", methods=["POST"])
 def analyse_starup():
     content = request.json
-
-
+    print(request.get_json())
+    print(content)
+    if "description" not in content:
+        response = jsonify([])
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+    
     document_result = client.embeddings.create(
         model = "solar-1-mini-embedding-passage",
-        input = content["description"]
+        input = content["description"] 
     )
 
     embedding = document_result.data[0].embedding
@@ -72,6 +79,9 @@ def analyse_starup():
     } for x in result]
 
     results_dict = {result["company"]: result for result in results}
+    response = jsonify(json.loads(json.htmlsafe_dumps(list(results_dict.values()))))
+    response.headers.add("Access-Control-Allow-Origin", "*")
 
-    return jsonify(list(results_dict.values()))
+    print(response.headers)
+    return response
 
